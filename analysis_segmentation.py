120cb251fedbac7d40c5bac617fe742e06677d9a
marianschoenauer <mschoenauer@outlook.at>
2026-02-18 17:40:32 +0100

    settings in loop. done for SLP

--- analysis_segmentation.py
+++ analysis_segmentation.py
@@ -22,101 +22,63 @@ SEG = True
 ROOT = ("D:/OneDrive - Mendelova univerzita v Brně/"
            "Coherence_VI_Krtiny/")
 
+FIGS, TABS = ROOT + "Manuscript/figures/", ROOT + "Manuscript/tables/"
+
 conc = xr.load_dataset(ROOT + "conc.nc", engine = "h5netcdf")
 
 conc = conc.assign(WI = conc['VV'] + conc['VH'])
 
-
-
-
-
-# %% setting
-
-cols_s2 = ['NDVI'] #'B3','B4','B5','B8A','B11','B12',
-cols_s1 = ['WI'] #'Rc','RVI',, 'VH','VV'
-cols_coherence = ['coh_12'] #,, 'coh_24', 'coh_36'
-
-sce_data = {'coherence':cols_coherence,
-            's1':cols_s1,
-           's1+coherence':cols_s1+cols_coherence,
-           's2':cols_s2,
-           's2+s1': cols_s1 + cols_s2,
-           's2+coherence': cols_s1+cols_s2+cols_coherence
-           }
-
-sce = pd.DataFrame({'cols': sce_data.values()}, index=sce_data.keys())
-
-del cols_s2, cols_s1, cols_coherence, sce_data
-
 # %%
-DS = []
 
-for Setting, cols in sce.iterrows():    
-    coh = conc[cols['cols']]
-    
-    month_before = pd.to_timedelta(-4, unit = 'W')
-    day_0 = pd.to_timedelta(0, unit = 'd')
-    month_after = pd.to_timedelta(4, unit = 'W')
-    
-    delay  = pd.to_timedelta(12, unit = 'd')
-        
-    Before = coh.sel({"time":slice(month_before, day_0)}).mean(dim = 'time')
-    After = coh.sel({"time":slice(delay, month_after)}).mean(dim = 'time')
-    
-    Mean = (Before - After)
-    Mean = Mean.fillna(Mean.mean())
-    DS.append(Mean)
+month_before = pd.to_timedelta(-4, unit = 'W')
+day_0 = pd.to_timedelta(0, unit = 'd')
+month_after = pd.to_timedelta(4, unit = 'W')
 
-DS = xr.concat(DS, dim = sce.index)
-del conc, month_before, month_after, day_0, delay, Before
-del After, Mean, cols, coh, Setting
+delay  = pd.to_timedelta(12, unit = 'd')
+    
+Before = conc.sel({"time":slice(month_before, day_0)}).mean(dim = 'time')
+After = conc.sel({"time":slice(delay, month_after)}).mean(dim = 'time')
+
+Mean = (Before - After)
+ds = Mean = Mean.fillna(Mean.mean())
+
+del month_before, day_0, month_after, delay, Before, After, Mean
+del conc
 
 # %%
 
 if SEG:
-    
-    DSseg = []
-    
-    for Setting in sce.index:
-        
-        ds = DS.sel(concat_dim = Setting)
-        
-        list_da = []
-        coords = ds.coords
-        
-        for feat_name in list(ds.data_vars):
-        
-            feat = feature.multiscale_basic_features(ds[feat_name].values,
-                                              intensity=True,
-                                              edges=False,
-                                              texture=True,
-                                              sigma_min=1,
-                                              sigma_max=16,
-                                              num_sigma = 5)
-            
-            np_name = np.array(feat_name)
-            np_sigmas = np.array(["1","2","3","4","5"])
-            
-            feat_names = np.concatenate((
-                np_name + np.array(["_intensity_"])+ np_sigmas,
-                np_name + np.array(["_texture1_"])+ np_sigmas,
-                np_name + np.array(["_texture2_"])+ np_sigmas))
-            
-            coords_i = coords.assign(feature = feat_names)
-            
-            
-            da_feat = xr.DataArray(feat, coords_i[['y','x','feature']].coords)
-            da_feat.name = feat_name
 
-            list_da.append(da_feat)        
-        
-        ds = xr.merge(list_da)
-        
-        DSseg.append(ds)
-        
-    DS = xr.concat(DSseg, dim = sce.index)
+    list_da = []
+    coords = ds.coords
     
-    del DSseg, Setting, ds, list_da, coords, feat, np_name, np_sigmas, feat_names, coords_i, da_feat
+    for feat_name in list(ds.data_vars):
+        feat_name
+    
+        feat = feature.multiscale_basic_features(ds[feat_name].values,
+                                          intensity=True,
+                                          edges=False,
+                                          texture=True,
+                                          sigma_min=1,
+                                          sigma_max=16,
+                                          num_sigma = 5)
+        
+        np_name = np.array(feat_name)
+        np_sigmas = np.array(["1","2","3","4","5"])
+        
+        feat_names = np.concatenate((
+            np_name + np.array(["_intensity_"])+ np_sigmas,
+            np_name + np.array(["_texture1_"])+ np_sigmas,
+            np_name + np.array(["_texture2_"])+ np_sigmas))
+        
+        coords_i = coords.assign(feature = feat_names)
+
+        da_feat = xr.DataArray(feat, coords_i[['y','x','feature']].coords)
+        da_feat.name = feat_name
+
+        list_da.append(da_feat) 
+    
+    ds = xr.merge(list_da)
 
 # %% assign TN and TP
 df_list = []
@@ -141,14 +103,10 @@ for PATH in glob.glob(ROOT + "clusters/TN/*.tif"):
         .to_dataframe(name = 'PlanSco')\
                     .dropna()\
                         .droplevel('band')
-                        
-    
-    
-    
-    
+                            
     
     if SEG:
-        sat = DS.transpose('concat_dim', 'feature', 'y', 'x')\
+        sat = ds.transpose('feature', 'y', 'x')\
             .rio.reproject_match(TN)\
                 .to_dataframe()\
                     .unstack(0)\
@@ -156,7 +114,7 @@ for PATH in glob.glob(ROOT + "clusters/TN/*.tif"):
                             .sort_index()
                         
     else:
-        sat = Mean.transpose('y', 'x')\
+        sat = ds.transpose('y', 'x')\
             .rio.reproject_match(TN)\
                 .to_dataframe()\
                     .swaplevel()
@@ -174,19 +132,39 @@ df_nc = pd.concat(df_list)\
     .set_index(['Gap'], append = True)\
         .sort_index()
 
-del df_list
+del df_list, tp_sat, tn_sat, sat, TN, TP, tp, tn
 
 # %% set index
 df = df_nc.loc[~df_nc.index.duplicated()].copy()
 df = df.reset_index()\
     .set_index(['Gap', 'AOI','x', 'y'])\
         .sort_index()\
-            .drop(columns = 'spatial_ref')
+            .drop(columns = 'spatial_ref')\
+                .dropna(how = 'all', axis = 1)
+                
+df = df.fillna(df.mean())
+# %% settings
 
-# %% models
+cols_s2 = ['NDVI','B8A','B11','B12',] #'B3','B4','B5'
+cols_s1 = ['WI', 'VH','VV', 'Rc'] #'Rc','RVI',
+cols_coherence = ['coh_12'] #,, 'coh_24', 'coh_36'
 
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.linear_model import RidgeClassifier as model
+sce_data = {'coherence':cols_coherence,
+            's1':cols_s1,
+           's1+coherence':cols_s1+cols_coherence,
+           's2':cols_s2,
+           's2+s1': cols_s1 + cols_s2,
+           's2+coherence': cols_s1+cols_s2+cols_coherence
+           }
+
+sce = pd.DataFrame({'cols': sce_data.values()}, index=sce_data.keys())
+
+del cols_s2, cols_s1, cols_coherence, sce_data
+
+# %% model training
+
+from sklearn.ensemble import RandomForestClassifier as model
+from sklearn.linear_model import RidgeClassifier 
 from sklearn.metrics import f1_score, fbeta_score
 from xgboost import XGBClassifier
 from sklearn.preprocessing import PolynomialFeatures
@@ -196,89 +174,100 @@ from sklearn.preprocessing import StandardScaler
 tests = []
 output_settings = []
 
-
 output_aois = []
 
 Partition = df.reset_index()['AOI'].isin(['train.tif']).values
-Partition.sum()
 
-if SEG:
+coefs = []
 
-    train = df.loc[Partition,ix[cols, :]].dropna(axis = 1, thresh = 0.8)\
-        .dropna().copy()
-    test = df.loc[~Partition,ix[cols, :]].dropna(axis = 1, thresh = 0.8)\
-        .dropna().copy()
-else:
-    train = df.loc[Partition,ix[cols]].dropna(axis = 1, thresh = 0.8)\
-        .dropna().copy()
-    test = df.loc[~Partition,ix[cols]].dropna(axis = 1, thresh = 0.8)\
-        .dropna().copy()
+for Setting, cols in sce.iterrows():
+   
+    train = df.loc[Partition,cols.iloc[0]].copy()
+    test = df.loc[~Partition,cols.iloc[0]].copy()
+    
+    train.columns.names = ['Feature', 'feature_seg']
 
-if np.any(test.reset_index(['Gap','AOI']).index.isin(
-        train.reset_index(['Gap','AOI']).index)):
-    print("OVERLAPPING PIXELS")
-
-y = train.reset_index().loc[:,'Gap'].values.flatten()
-X = train.loc[:,cols]
-
-pos_weight = df.groupby('Gap').size()[False] / df.groupby('Gap').size()[True]
-weights = np.where(y, pos_weight, 1)
-
-pipe = Pipeline([('scaler',StandardScaler()),
-                 #('poly', PolynomialFeatures()),
-                 ('model',model(random_state = 1, solver = 'auto'))]) 
-#, scale_pos_weight = pos_weight
-
-model_fit = pipe.fit(X = X.to_numpy(), y = y,
-                     model__sample_weight = weights)
-test['pred_' + Setting] = model_fit.predict(test.loc[:,cols])
-
-tests.append(test)
-
-del train, test, y, X, pos_weight, weights, pipe, model_fit
-
+    if np.any(test.reset_index(['Gap','AOI']).index.isin(
+            train.reset_index(['Gap','AOI']).index)):
+        print("OVERLAPPING PIXELS")
+    
+    y = train.reset_index().loc[:,'Gap'].values.flatten()
+    X = train.to_numpy()
+    
+    pos_weight = df.groupby('Gap').size()[False] / df.groupby('Gap').size()[True]
+    weights = np.where(y, pos_weight, 1)
+    
+    pipe = Pipeline([('scaler',StandardScaler()),
+                     ('model',model(random_state = 1))]) 
+    #solver = 'auto', scale_pos_weight = pos_weight
+    
+    model_fit = pipe.fit(X = X, y = y,
+                         model__sample_weight = weights)
+    test['pred_' + Setting] = model_fit.predict(test)
+    tests.append(test.loc[:,['pred_' + Setting]])
+    
+    #coef = pd.DataFrame({"weight": model_fit['model'].coef_},
+     #                   index = train.columns)\
+      #  .assign(Setting = Setting)
+    
+    #coefs.append(coef)
+    
+    del train, test, y, X, pos_weight, weights, pipe, model_fit
+    
 Test = pd.concat(tests, axis=1)
 Test.index = Test.index.remove_unused_levels()
+# %%
+cc = pd.concat(coefs)\
+    .set_index('Setting', append = True)\
+        .reorder_levels([2,0,1])\
+            .sort_index()
 
+cc.unstack('Setting').to_excel(TABS + 'model_weights.xlsx')
 # %% validation metrics
 
 from sklearn.metrics import matthews_corrcoef, precision_score
 
 results_val = []
 
+for (aoi), group in Test.groupby('AOI'):
+    
+    y_true = group.reset_index(['Gap'])['Gap'].values
+    
+    for Setting in group.columns:
+        Setting
+        
+        y_pred = group.loc[:,Setting].values
+        
+        F1 = f1_score(y_true = y_true, y_pred = y_pred, zero_division = 0)
+        F05 = fbeta_score(y_true = y_true, y_pred = y_pred, beta = 0.5, \
+                          zero_division = 0)
+        Precision = precision_score(y_true = y_true, y_pred = y_pred, \
+                                    zero_division = 0)
+        MCC = matthews_corrcoef(y_true = y_true, y_pred = y_pred)
+        
+        results_val.append({'setting':Setting[0][5:],
+                        'test_area':aoi,
+                        'F1':F1,
+                        'F05': F05,
+                        'Precision':Precision,
+                        'MCC':MCC})
+    
 
-for AOI in Test.index.levels[1].values:
-
-    y_true = np.array(Test.loc[ix[:,AOI],:].index.get_level_values('Gap'),\
-                      dtype = np.bool_)
-    y_pred = np.array(Test.loc[ix[:,AOI],"pred_"+Setting].values.flatten(),\
-                      dtype = np.bool_)
-
-    F1 = f1_score(y_true = y_true, y_pred = y_pred, zero_division = 0)
-    F05 = fbeta_score(y_true = y_true, y_pred = y_pred, beta = 0.5, \
-                      zero_division = 0)
-    Precision = precision_score(y_true = y_true, y_pred = y_pred, \
-                                zero_division = 0)
-    MCC = matthews_corrcoef(y_true = y_true, y_pred = y_pred)
-
-    results_val.append({'setting':Setting,
-                    'test_area':AOI,
-                    'F1':F1,
-                    'F05': F05,
-                    'Precision':Precision,
-                    'MCC':MCC})
-
-VAL = pd.DataFrame(results_val).set_index(['setting','test_area'])
+VAL = pd.DataFrame(results_val).set_index(['setting','test_area'])\
+    .sort_index()
 VAL.columns.name = 'Metric'
 
 sns.boxplot(VAL.stack(level = 'Metric').to_frame(name = "value"), \
             x = 'setting', y = 'value', hue = 'Metric')
+plt.savefig(FIGS + 'val_metrics_boxplot.png')
 plt.show()
 
 from scipy.stats import ttest_rel
 
 #print('F05', VAL.groupby('setting')['F05'].describe())
 print('F1', VAL.groupby('setting')['F1'].describe())
+
+VAL.to_excel(ROOT+"Manuscript/tables/val_metrics.xlsx")
 
 # %%
 """
@@ -310,19 +299,26 @@ for AOI in Test.index.levels[1]:
             .rio.write_crs(CRS)\
                 .rio.reproject(4326)
 
-    fig, ax = plt.subplot_mosaic([['Gap','coh'],
-                                  ['S1', 'coh_S1']], figsize = (12,10))
+    fig, ax = plt.subplot_mosaic([
+                                  ['Gap', 'Gap', 'coherence', 's1', 's1+coherence'],
+                                  ['Gap', 'Gap', 's2', 's2+s1','s2+coherence']], figsize = (12,6),
+                                 constrained_layout = True)
 
-    ds['Gap'].plot(ax = ax['Gap'])
+    ds['Gap'].plot(ax = ax['Gap'], add_colorbar = False)
+    ax['Gap'].set_title('ground truthing')
     
-    
-    ds['pred_'+Setting].plot(ax = ax['coh'])
-    #ds['pred_s2'].plot(ax = ax['S1'])
-    #ds['pred_s1+coherence'].plot(ax = ax['coh_S1'])
-
-    ax['Gap'].set_title('Gap ground-truth')
-    #ax['coh'].set_title('Pred.: coherence')
-    #ax['S1'].set_title('Pred.: WI =delta(VV+VH')
-    #ax['coh_S1'].set_title('Pred.: WI+Coherence')
-    plt.show()
-    #ds.rio.to_raster(ROOT + "preds/" + AOI)
+    for Setting in sce.index:
+        Setting
+        
+        ds['pred_' + Setting].plot(ax = ax[Setting], add_colorbar = False)
+        ax[Setting].set_title(Setting)
+        ax[Setting].set_yticks([])
+        ax[Setting].set_xticks([])
+        
+        F1 = VAL.loc[(Setting, AOI),"F1"].round(2)
+        ax[Setting].annotate('F1 = ' + str(F1), xy = (0.1,0.9), 
+                             xycoords = 'axes fraction',
+                             bbox=dict(facecolor='white', alpha=0.8, edgecolor='none'))
+        
+    fig.savefig(FIGS + AOI + ".png", dpi = 400)
+        
